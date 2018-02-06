562cf8d9ff8c84c256b4bc14b37215800e8f4790
Jairo Vera <jairov1995@yahoo.com>
2018-02-06 09:20:39 -0500

    logica de preguntas

--- poliapp/src/pages/home/home.ts
+++ poliapp/src/pages/home/home.ts
@@ -7,8 +7,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  puntuacion: number;
+  all_preguntas: Pregunta[]=[];
 
+
+
+  constructor(public navCtrl: NavController) {
+    
   }
 
+  ionViewWillEnter(){
+
+    this.puntuacion = 0;
+
+    this.all_preguntas.push(new Pregunta('Cuantos años tienes?','Nose bro disculpa','Tigre dientes de sable','Elefantes sobre agua','alais chupa sangre','A',100));
+    this.all_preguntas.push(new Pregunta('Cuantos años tienes?','12','13','14','15','B',75));
+    this.all_preguntas.push(new Pregunta('Cuantos años tienes?','12','13','14','15','C',50));
+    this.all_preguntas.push(new Pregunta('Cuantos años tienes?','12','13','14','15','D',25));
+  }
+
+  get_score(){
+    this.puntuacion=0;
+    for (let item of this.all_preguntas){
+      if (item.selected === item.respuesta)
+        this.puntuacion+=item.puntos;
+    }
+  }
+
+}
+
+class Pregunta{
+  constructor(public pregunta:string,
+  public optionA:string,
+  public optionB:string,
+  public optionC:string,
+  public optionD:string,
+  public respuesta:string,
+  public puntos:number,
+  public selected?:string){
+  }
 }
